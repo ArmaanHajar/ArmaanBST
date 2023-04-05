1f8bcba8cf8d0a1fdd91a0b8abfe8e7ea8631305
ArmaanHajar <armaanhajar@icloud.com>
2023-04-05 09:39:48 -0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+/*
+ * Author: Armaan Hajar
+ * Date: 
+ * Description: Binary Search Tree Data Structure
+ * Used the Wikipedia to learn how to implement a binary search tree
+ * Link: https://en.wikipedia.org/wiki/Binary_search_tree
+ */
 
+#include <iostream>
+
+using namespace std;
+
+// Binary Tree struct
+struct BinaryTree {
+    char data;
+    BinaryTree* right;
+    BinaryTree* left;
+    BinaryTree(char newData) {
+        data = newData;
+        right = NULL;
+        left = NULL;
+    }
+};
