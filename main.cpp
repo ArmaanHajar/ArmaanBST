c73d523bc23a20c711469ceee6440dec935a0f7c
ArmaanHajar <armaanhajar@icloud.com>
2023-04-09 17:25:00 -0700

    insert and print working

--- main.cpp
+++ main.cpp
@@ -13,22 +13,23 @@ using namespace std;
 
 // Binary Tree struct
 struct BinaryTree {
-    char data;
+    int data;
     BinaryTree* right;
     BinaryTree* left;
-    BinaryTree(char newData) {
+    BinaryTree(int newData) {
         data = newData;
         right = NULL;
         left = NULL;
     }
 };
 
-void insert(BinaryTree* tree, char newData);
-void print(BinaryTree* tree);
+void insert(BinaryTree* &tree, BinaryTree* newData);
+void print(BinaryTree* tree, int indent);
 
 int main() {
     char input[10];
     bool running = true;
+    BinaryTree* tree = NULL;
 
     while (running == true) {
         cout << "------------------------------------------------------------------" << endl;
@@ -37,7 +38,7 @@ int main() {
         cin.get(input, 10);
         cin.ignore(1, '\n');
 
-        if (input[1] == 'I' || input[1] == 'i') { // inputs numbers into the tree
+        if (input[0] == 'I' || input[0] == 'i') { // inputs numbers into the tree
             int inputNum;
             bool done = false;
             cout << "Begin Inputting Numbers Between 1-999, Type '0' When Done" << endl;
@@ -45,34 +46,41 @@ int main() {
                 cin >> inputNum;
                 cin.ignore(1, '\n');
                 if (inputNum >= 1 && inputNum <= 999) {
-                    insert(tree, inputNum);
+                    BinaryTree* newNode = new BinaryTree(inputNum);
+                    insert(tree, newNode);
                 }
                 else if (inputNum == 0) {
-                    done = true;
                     cout << "Done Inputting Numbers" << endl;
+                    done = true;
                 }
                  else {
                     cout << "Invalid Input" << endl;
                 }
             }
         }
-        if (input[1] == 'G' || input[1] == 'g') { // generates numbers from a file
+        if (input[0] == 'G' || input[0] == 'g') { // generates numbers from a file
+            cout << "Generating Numbers From File" << endl;
             ifstream numbers;
             numbers.open("numbers.txt");
-            // code to take number individually from file and add to tree
+            int newNum;
+            while (numbers >> newNum) { // loops until end of file
+                BinaryTree* newNode = new BinaryTree(newNum);
+                insert(tree, newNode);
+            }
             numbers.close();
         }
-        if (input[1] == 'P' || input[1] == 'p') { // prints tree in order
-            print(tree);
+        if (input[0] == 'P' || input[0] == 'p') { // prints tree
+            cout << "Printing Tree" << endl;
+            print(tree, 0);
         }
-        if (input[1] == 'D' || input[1] == 'd') { // deletes tree or value from the tree
+        if (input[0] == 'D' || input[0] == 'd') { // deletes tree or value from the tree
 
         }
-        if (input[1] == 'Q' || input[1] == 'q') { // quits the program
+        if (input[0] == 'Q' || input[0] == 'q') { // quits the program
             cout << "Thank you for using the Binary Search Tree Program!" << endl;
             running = false;
         }
-        if (input[1] == 'H' || input[1] == 'h') {
+        if (input[0] == 'H' || input[0] == 'h') {
             cout << "INPUT: Allows you to input numbers to be added to the tree" << endl;
             cout << "GENERATE: Generates numbers from a file of numbers to be added to the tree" << endl;
             cout << "PRINT: Prints the tree in order" << endl;
@@ -84,40 +92,29 @@ int main() {
 }
 
 // left is smaller, right is bigger
-void insert(BinaryTree* tree, char newData) {
+void insert(BinaryTree* &tree, BinaryTree* newData) {
     if (tree == NULL) {
-        tree = new BinaryTree(newData);
+        tree = newData;
     }
-    else if (newData < tree->data) {
-        if (tree->left != NULL) {
-            insert(tree->left, newData);
-        }
-        else {
-            tree->left = new BinaryTree(newData);
-        }
+    else if (newData->data < tree->data) {
+        insert(tree->left, newData);
     }
-    else if (newData > tree->data) {
-        if (tree->right != NULL) {
-            insert(tree->right, newData);
-        }
-        else {
-            tree->right = new BinaryTree(newData);
-        }
+    else if (newData->data > tree->data) {
+        insert(tree->right, newData);
     }
 }
 
-void print(BinaryTree* tree) {
-    int indent;
-    if (tree == NULL){
+void print(BinaryTree* tree, int indent) {
+    if (tree == NULL) {
         return;
     }
     indent += 10;
 
-    print(tree->right);
+    print(tree->right, indent);
     cout << endl;
     for (int i = 10; i < indent; i++) {
         cout << " ";
     }
-    cout << tree->data << "\n";
-    print(tree->left);
+    cout << tree->data << endl;
+    print(tree->left, indent);
 }
